ed8903c160513caea7d9c50f53a4ec4ad6b0f60f
silvamoraiskalebe4@gmail.com <kalebe53602596@alunos.to.senac.br>
2026-09-02 17:55:47 -0300

    aprendendo sobre variaveis

---
+++ Variaveis/src/main/java/com/mycompany/variaveis/Variaveis.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+//duas barra cmentario de uma(1) liha so
+ 
+/*
+barra e asteristico comnetario
+ de varias
+lnhas
+*/
+package com.mycompany.variaveis;
+
+/**
+ *
+ * @author KalebeMorais
+ */
+public class Variaveis {
+       // a classe main é quem executa os nossa códigos
+    public static void main(String[] args) {
+ /*código de saida
+        -são utlizados para imprimir/mostrar dados e mensagens.
+        */
+ System.out.println("Ola");
+ System.out.println("eu sou um progamador");  
+//println - pula linha / print - nao pula 
+    System.out.print("eu ja sei progamar");
+    System.out.print("eu nao amo o java");
+// criacao de variaveis
+
+    int idade = 16;// volor inteiro
+    double altura = 1.70;// valor decimal grande
+    double peso = 85.4;// valor decimmal pequeno 
+    String nome = "kalebe silva";// texto
+    boolean casada = false;// vlaor logico //true - verdadeiro , false -falso   
+     System.out.println("Ola"+nome);
+     System.out.println("voce tem"+idade+"anos de idade");}
+}
+// nem um codigo pode ficar aqui fora   
